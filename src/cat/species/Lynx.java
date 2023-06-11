c046bbada0f0690b33b8589b0e6e374a3dbbf297
Pratibha Chauhan <pratibhanaruka8@gmail.com>
2023-06-11 19:28:44 +0530

    Modifiers done

---
+++ src/cat/species/Lynx.java
@@ -0,0 +1,18 @@
+package cat.species;
+
+import cat.BigCat;
+
+public class Lynx extends BigCat{
+public static void main (String [] args)
+	
+	{
+		BigCat c = new BigCat();
+		Lynx obj = new Lynx();
+		System.out.println(c.name);
+		System.out.println(c.hasFur);
+		System.out.println(c.hasPaws);
+		System.out.println(c.id);
+		
+	}
+
+}
